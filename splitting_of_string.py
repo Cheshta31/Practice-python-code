7d46df82cc0d0de915d45b69a8e004c5193c18b4
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-04-22 13:41:35 +0530

    Add files via upload

---
+++ splitting_of_string.py
@@ -0,0 +1,10 @@
+#not a user defined program for splitting
+a="Cheshta Khurana"
+x=a.split("e")
+print("After splitting string = ",x)
+#user defined program for splitting
+str1=input("Enter a string")
+n=input("Enter a character from where splitting to begin")
+str2=str1.split(n)
+print("After splitting string = ",str2)
+
