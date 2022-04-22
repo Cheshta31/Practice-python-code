b8d5cb3719699b31e5c88168a22a2bb8fd94f44f
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-04-22 13:42:51 +0530

    Add files via upload

---
+++ multiplication_of_string.py
@@ -0,0 +1,10 @@
+#not a user defined program
+x="Cheshta"
+y=8
+z=x*y
+print("After multiplication string = ",z)
+#user defined program
+str1=input("Enter a string")
+n=int(input("Enter the no. of times you want to multiply"))
+str2=str1*n
+print("String multiplying = ",str2)
