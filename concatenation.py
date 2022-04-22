56a3fc29d71a336c0465bdca51fb95035280e763
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-04-22 13:33:47 +0530

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,9 @@
+#Not a user defined program for concatenation
+a="Cheshta"
+b="Khurana"
+c=a+b
+print("After concatenation string = ",c)
+#user defined program for concatenation
+str1=input("Enter 1st string")
+str2=input("Enter 2nd string")
+print("String after concatenation = ",str1," " +str2)
