9f25f2635961d7c1b479b68aeeb45293b9c9a5da
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-04-23 09:21:43 +0530

    Add files via upload

---
+++ addition_program.py
@@ -0,0 +1,18 @@
+#write a non-user-defined python script to add two no.
+print("To add two no.")
+a=5
+b=8
+c=a+b
+print(c)
+#write a user-defined-python script to add two integer no.
+print("To add two integer no. ")
+a=int(input("Enter 1st no. = "))
+b=int(input("Enter 2nd no. = "))
+c=a+b
+print("Addition of two no. = ",c)
+#write a user-defined-python script to add two float no.
+print("To add two floating point no. ")
+a=float(input("Enter 1st no. = "))
+b=float(input("Enter 2nd no. = "))
+c=a+b
+print("Addition of two no. = ",c)
