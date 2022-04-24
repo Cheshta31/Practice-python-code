c92079e11d91f232e1fca9640dee2bc25d320ed1
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-04-24 13:21:22 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,10 @@
+#program for swapping
+n=int(input("Enter a no. = "))
+m=int(input("Enter another no. = "))
+print("Values before swapping")
+print("First =",n)
+print("Second =",m)
+n,m=m,n
+print("Values after swapping")
+print("First",n)
+print("Second",m)
