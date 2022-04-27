30d736830694872c64302d45040f45ee94a98a51
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-04-27 21:00:39 +0530

    Add files via upload

---
+++ accessing_ele_in_list.py
@@ -0,0 +1,12 @@
+#lists
+list_1=[1,2,3,'Eureka','Cool']
+list_2=[8,9,21,'Python','Fun']
+print(list_1)
+print(list_2)
+#accessing elements in list
+print(list_1[:])                       #accessing all elements at a go
+print(list_1[3])     # accessing 3 elements from the list
+print(list_1[0:4])      #accessing elements from 1 to 3,leaving 0 and 4
+print(list_1[::-1]) #print in reverse order
+print(list_1[0:5:2])
+print(list_1[3][2]) # access particular element of string
