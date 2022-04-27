b6009165456f8121140b2358f83f4077290176ae
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-04-27 20:59:43 +0530

    Add files via upload

---
+++ implicit_type_casting.py
@@ -0,0 +1,10 @@
+#program for implicit type casting
+a=88
+b=78
+c=9.812
+d=8.321
+e=89+9j
+f=78+10j
+print('Addition of an int and a float values',(a+c))
+print('Addition of an float and complex no.',(b+f))
+print('Multiplication of an int and float',int(b*d))
