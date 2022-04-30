e3ae926de04487adc58c3fa3d4f702a37db24a6d
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-04-30 10:43:04 +0530

    Add files via upload

---
+++ posi_negi_no.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter a no."))
+if n1<0:
+    print("Negative no.")
+elif n1>0:
+    print("Positive no.")
+elif n==0:
+    print("No. is neither positive nor negative")
+else:
+    print("Number was not entered")
+    
