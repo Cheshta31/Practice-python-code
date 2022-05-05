63e298c9f1e7b98f21a9d6ca9ee652e5b70350ab
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-05-05 23:41:56 +0530

    Add files via upload

---
+++ dicitionary.py
@@ -0,0 +1,10 @@
+#program for dictionary
+dictionary={
+    "car 1":"Hexa"
+    "car 2":"Ertiga"
+    "car 3":"I 10"
+    "car 4":"I 20"
+    }
+print(dictionary)
+print(dictionary["car 3"])
+print("Length of this dictionary = ",len(dictionary))
