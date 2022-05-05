6ce554c885998c6be823879bed4fcc2b2aacdc2e
Cheshta31 <99382930+Cheshta31@users.noreply.github.com>
2022-05-05 23:39:26 +0530

    Add files via upload

---
+++ swap_using_3_variable.py
@@ -0,0 +1,20 @@
+#swap 2 no. using third variable method
+print("This script is intended to swap the values of two data variables using a temporary variable")
+a=input("Enter a no. = ")
+b=input("Enter another no. = ")
+print("Values before swappinng :- ")
+'''print("First no. = ",a)
+print("Second no. = ",b)
+'''
+print("First ="+str(a))
+print("Second ="+str(b))
+c=a
+a=b
+b=c
+print("After swapping")
+'''
+print("First no.",a)
+print("Second no.",b)
+'''
+print("first no.="+str(a))
+print("second no.="+str(b))
